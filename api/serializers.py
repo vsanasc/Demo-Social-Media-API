a344f59cbf2740df3c9b9a57cd27dd2655a30814
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2021-10-17 18:16:44 -0300

    Add serializers: Post, Attachment and Comment

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from app.models import Profile
+from app.models import Profile, Post, Comment, Attachment
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,3 +15,22 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('birthdate','bio',)
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+class AttachmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attachment
+        fields = ('file','is_image',)
+
+class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    attachments = AttachmentSerializer(many=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'user', 'text', 'attachments', 'created_at', 'modified_at',)
