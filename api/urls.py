98e23abf5496d174d6644a5d4b1af262c368fb5f
Vitor Santos do Nascimento <vsantos.nasc@gmail.com>
2021-10-17 18:17:50 -0300

    Add feed API

--- api/urls.py
+++ api/urls.py
@@ -7,10 +7,12 @@ from rest_framework_simplejwt.views import (
 
 from .profile import MyProfileAPI, ProfileAPI
 from .signup import SignupAPI
+from .feed import FeedAPI
 
 urlpatterns = [
     path('sign-up/', SignupAPI.as_view()),
     path('my-profile/', MyProfileAPI.as_view()),
+    path('feed/', FeedAPI.as_view()),
     path('friendship/', include('api.friendship.urls')),
     path('profile/<int:id>/', ProfileAPI.as_view()),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
